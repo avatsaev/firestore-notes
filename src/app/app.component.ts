a1a23d181b1082fb26ee4bb24fb47f6ed6efa971
Aslan Vatsaev <aslan.vatsaev@ihu-strasbourg.eu>
2017-10-16 15:47:02 +0200

    feat: step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Note} from './models/note';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello World</h1>
+    <h1>Notes:</h1>
+      
+      <ul>
+        <li *ngFor="let note of notes">{{note.title}}: {{note.content}}</li>
+      </ul>
     
   `,
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  notes: Note[];
+
+  constructor() {}
+
+
+  ngOnInit() {
+
+    this.notes = [
+      {
+        id: 1,
+        title: 'Note 1',
+        content: 'My note 1'
+      },
+      {
+        id: 1,
+        title: 'Note 2',
+        content: 'My note 2'
+      },
+      {
+        id: 1,
+        title: 'Note 3',
+        content: 'My note 3'
+      }
+    ]
+  }
+
 }
---
+++ src/app/models/note.ts
@@ -0,0 +1,5 @@
+export interface Note {
+  id?: string | number;
+  title: string;
+  content: string;
+}
