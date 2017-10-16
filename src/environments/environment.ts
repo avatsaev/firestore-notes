75aa213c28d377841162f9073573fe2fd47ae847
Aslan Vatsaev <aslan.vatsaev@ihu-strasbourg.eu>
2017-10-16 15:27:58 +0200

    feat: init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConf : {
+    apiKey: "AIzaSyAL-FzQ1pdt6SB-cPYGDbVdSRITEf7glQM",
+    authDomain: "firestore-test-134fe.firebaseapp.com",
+    databaseURL: "https://firestore-test-134fe.firebaseio.com",
+    projectId: "firestore-test-134fe",
+    storageBucket: "",
+    messagingSenderId: "829161237828"
+  }
 };
