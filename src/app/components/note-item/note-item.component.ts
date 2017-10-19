2e64a18e210acd88985ac22d960ccea3c72fdc2b
Aslan Vatsaev <aslan.vatsaev@ihu-strasbourg.eu>
2017-10-19 13:56:40 +0200

    chore: step 2

---
+++ src/app/components/note-item/note-item.component.ts
@@ -0,0 +1,46 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Note} from '../../models/note';
+
+@Component({
+    selector: 'app-note-item',
+    template: `
+        <div class="card">
+            <header class="card-header">
+        <span class="card-header-title">
+          {{note.title}}
+        </span>
+                <a href="#" class="card-header-icon" aria-label="more options">
+      <span class="icon">
+        <i class="fa fa-angle-down" aria-hidden="true"></i>
+      </span>
+                </a>
+            </header>
+            <div class="card-content">
+                <div class="content">
+                    {{note.content}}
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a (click)="onEdit.emit(note)" class="card-footer-item">Edit</a>
+                <a (click)="onDelete.emit(note)" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
+    `,
+    styles: [`
+      .card{
+        margin-bottom: 10px;
+      }
+     
+  `]
+})
+export class NoteItemComponent implements OnInit {
+
+    @Input() note: Note;
+    @Output() onEdit = new EventEmitter<Note>();
+    @Output() onDelete = new EventEmitter<Note>();
+
+    constructor() { }
+
+    ngOnInit() {}
+
+}
